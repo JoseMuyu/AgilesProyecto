f9da85b96d627e7f4bd5060067f55dfdccdeded6
JoseElias <jmuyulema9096@uta.edu.ec>
2023-06-25 01:15:04 -0500

    Estructura basica del proyecto creada

---
+++ Admin/Agiles_ProyectoSemestral_ProgramaAdmin_SmartHealth/src/main/java/Principal/Agiles_ProyectoSemestral_ProgramaAdmin_SmartHealth.java
@@ -0,0 +1,8 @@
+package Principal;
+
+public class Agiles_ProyectoSemestral_ProgramaAdmin_SmartHealth {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
